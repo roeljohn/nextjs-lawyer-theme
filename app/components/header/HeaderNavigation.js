b3ee6c36d5776a999113d015b1a06e2ffe12e40c
roeljohnbobis <roeljohn@users.noreply.github.com>
2024-04-10 21:13:03 +0800

    create navigation component with api

--- app/components/header/HeaderNavigation.js
+++ app/components/header/HeaderNavigation.js
@@ -1,21 +1,11 @@
-const HeaderNavigation = ({  }) => {
+const HeaderNavigation = ({ menu_items }) => {
     return (
-        <ul class="navbar-nav w-100 justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link" href="https://roeljohn.github.io/lawyer-psd-html/">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://roeljohn.github.io/lawyer-psd-html/about">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://roeljohn.github.io/lawyer-psd-html/services">Services</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://roeljohn.github.io/lawyer-psd-html/blog">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://roeljohn.github.io/lawyer-psd-html/contact">Contact</a>
-            </li>
+        <ul class="navbar-nav w-100 justify-content-center">     
+            {menu_items && menu_items.map((menu, index) => (
+                <li class="nav-item">
+                    <a class="nav-link" href={menu.url}>{menu.title.rendered}</a>
+                </li>
+            ))}
         </ul>
     );
 };
